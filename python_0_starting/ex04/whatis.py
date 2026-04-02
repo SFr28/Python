6ce0931b0330f4776a9694c00291bb9b8264665a
saina fraslin <sfraslin@f1br6s10.paris.42.school>
2026-04-02 19:16:21 +0200

    ex04

---
+++ python_0_starting/ex04/whatis.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) > 2:
+	print ("AssertionError: more than one argument is provided")
+	sys.exit()
+
+try:
+	nb = int(sys.argv[1])
+	if (nb % 2 == 0):
+		print ("I'm Even.")
+	else:
+		print ("I'm Odd.")
+except:
+	print("AssertionError: argument is not an integer")
+	sys.exit()
