b2eb115ab85653bc8847850136b48ed373fc2e47
saina fraslin <sfraslin@f1r4s23.paris.42.school>
2026-04-15 18:08:56 +0200

    ex07

---
+++ python_0_starting/ex07/sos.py
@@ -0,0 +1,53 @@
+import sys
+
+MORSE = {' ': '/',
+		'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..'
+}
+
+def main():
+	if (len(sys.argv) != 2):
+		print("AssertionError: the arguments are bad")
+		sys.exit()
+
+	string = sys.argv[1].upper()
+	code = []
+	for char in string:
+		if char in MORSE.keys():
+			code.append(MORSE[char])
+		else:
+			print("AssertionError: the arguments are bad")
+			sys.exit()
+	
+	for char in code:
+		print(char, end=" ")
+	print()
+
+
+
+if __name__ == "__main__":
+	main()
