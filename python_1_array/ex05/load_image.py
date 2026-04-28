476e8936249817791332dea1a58f99a3e79bf656
sfraslin <sfraslin@f1r9s2.paris.42.school>
2026-04-28 19:21:59 +0200

    ex05

---
+++ python_1_array/ex05/load_image.py
@@ -0,0 +1,19 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ft_load(path: str) -> np.ndarray:
+	'''Loads an image with matplotlib and returns an array with the RGB content'''
+	
+	img = mpimg.imread(path)
+	array = np.asarray(img)
+
+	print("The shape of the image is: ", array.shape)
+	print (array)
+
+	plt.subplot(3, 2, 1)
+	plt.imshow(array)
+	plt.axis('off')
+	plt.title('Figure VIII.1: Original', y=-0.25)
+
+	return array
