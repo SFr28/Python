6255e2114a920cabec17d0df520653bdf63214d4
sfraslin <sfraslin@f1br6s15.paris.42.school>
2026-04-29 12:27:12 +0200

    python_2 ex00

--- python_2_DataTable/ex00/load_csv.py
+++ python_2_DataTable/ex00/load_csv.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
-def load(path: str) -> Dataset: (You have to adapt the type of return according to your library)
-    #your code here
+def load(path: str) -> pd.DataFrame:
+    '''Loads a csv doc with a data set, \
+    writes its dimensions and returns it'''
+
+    try:
+        df = pd.read_csv(path)
+        print("Loading dataset of dimensions", df.shape)
+        return df
+    except FileNotFoundError:
+        print("No such file or directory:", path)
+    except (ValueError, TypeError) as e:
+        print(e)
+        return None
