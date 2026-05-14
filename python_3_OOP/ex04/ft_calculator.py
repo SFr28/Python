63708e263d09db6740519cf666c2d40293d48f45
sfraslin <sfraslin@f1br5s5.paris.42.school>
2026-05-14 11:33:21 +0200

    ex04 using zip

--- python_3_OOP/ex04/ft_calculator.py
+++ python_3_OOP/ex04/ft_calculator.py
@@ -3,22 +3,26 @@ class calculator:
     Calculator class to calculate dot product, addition and substraction of vectors
     """
 
-    @classmethod
+    @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         """Calculating dot product : a · b = ax × bx + ay × by"""
-        dot = 0
-        for i in V1:
-            dot += V1[i] * V2[i]
-        print("Dot product is: " + dot)
+        dot = zip(V1, V2)
+        print(f"Dot product is: {sum(i*j for (i, j) in dot)}")
 
-    @classmethod
+    @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
         """Calculating addition"""
-        add = [V1[i] + V2[i] for i in V1]
-        print("Dot product is: " + add)
+        add = zip(V1, V2)
+        print(f"Add vector is: {[float(i + j) for (i, j) in add]}")
 
-    @classmethod
+    @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
         """Calculating subtraction"""
-        add = [V1[i] - V2[i] for i in V1]
-        print("Dot product is: " + sub)
+        print(f"Sous Vector is: {[float(i - j) for (i, j) in zip(V1, V2)]}")
+
+
+# The zip() function returns a zip object, which is an iterator of tuples 
+# where the first item in each passed iterator is paired together, and 
+# then the second item in each passed iterator are paired together etc.
+# If the passed iterables have different lengths, the iterable with the 
+# least items decides the length of the new iterator.
