128a5a6afa07cb770eb369ec0472f436a8a622ac
sfraslin <sfraslin@f1r2s13.paris.42.school>
2026-06-11 15:53:13 +0200

    reorganisation ex09

--- python_0_starting/ex09/src/ft_package/__init__.py
+++
@@ -1,4 +0,0 @@
-from ft_package.list_utils import count_in_list, index_in_list
-from ft_package.math_utils import percent
-
-__all__ = ["count_in_list", "index_in_list", "percent"]
--- python_0_starting/ex09/src/ft_package/math_utils.py
+++
@@ -1,4 +0,0 @@
-def percent(nb, total) -> int:
-    "Returns the percentage corresponding to the number and total given"
-
-    return (nb / total * 100)
