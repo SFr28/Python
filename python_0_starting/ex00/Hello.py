a0dae6d72223a9e2815f0c008fd86f47a3889f37
sfraslin <sfraslin@f1r2s13.paris.42.school>
2026-06-11 11:51:41 +0200

    adding comments to starting_ex00

--- python_0_starting/ex00/Hello.py
+++ python_0_starting/ex00/Hello.py
@@ -1,20 +1,26 @@
-# ft_list = ["Hello", "tata!"]
-# ft_tuple = ("Hello", "toto!")
-# ft_set = {"Hello", "tutu!"}
-# ft_dict = {"Hello" : "titi!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
 
+#List: List items are ordered, changeable, and allow duplicate values
 ft_list[1] = "World!"
 
+#Tuple: Tuple items are ordered, unchangeable, and allow duplicate values.
 tmp = list(ft_tuple)
 tmp[1] = "France!"
 ft_tuple = tuple(tmp)
 
+#Set: Set items are unordered, unchangeable, and do not allow duplicate values
+#Contrary to tuple, we can still add and remove items in a set
 ft_set.remove("tutu!")
 ft_set.add("Paris!")
 
+#Dictionary: Dictionary items are ordered, changeable, and do not allow duplicates
+#Dictionary items are presented in key:value pairs, and can be referred to by using the key name
 ft_dict["Hello"] = "42Paris!"
 
-# print(ft_list)
-# print(ft_tuple)
-# print(ft_set)
-# print(ft_dict)
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
