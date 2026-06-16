00b15df703e2f2d268c9fab95ea4af58d4215617
sfraslin <sfraslin@f1r2s16.paris.42.school>
2026-06-16 17:46:26 +0200

    adding try except to ex02 and tester

--- python_1_array/ex02/load_image.py
+++ python_1_array/ex02/load_image.py
@@ -5,9 +5,17 @@ import numpy as np
 def ft_load(path: str) -> np.ndarray:
     '''Loads an image with matplotlib and returns \
     an array with the RGB content'''
-    img = mpimg.imread(path)
-    array = np.asarray(img)
+    try:
+        if path.find(".jpg") != (len(path) - 4) and path.find(".jpeg") != (len(path) - 5):
+            raise TypeError("not a jpg or jpeg")
+        img = mpimg.imread(path)
+        array = np.asarray(img)
 
-    print("The shape of the image is: ", array.shape)
+        print("The shape of the image is: ", array.shape)
+        return array
 
-    return array
+    except TypeError as e:
+        print("Type error:", e)
+    except Exception as e:
+        print("Error:", e)
+
